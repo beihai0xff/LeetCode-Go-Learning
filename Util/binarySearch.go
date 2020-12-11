8eb507a33065ef73895a04fa673c2b03749cc533
beihai <beihai@wingsxdu.com>
2020-12-11 19:32:45 +0800

    添加二分搜索的应用示例

--- Util/binarySearch.go
+++ Util/binarySearch.go
@@ -34,3 +34,38 @@ func binarySearch(nums []int, target int) int {
 // 2. 查找最后一个与 target 相等的元素，时间复杂度 O(logn)
 // 3. 查找第一个大于等于 target 的元素，时间复杂度 O(logn)
 // 4. 查找最后一个小于等于 target 的元素，时间复杂度 O(logn)
+
+// 二分查找第一个与 target 相等的元素
+func searchFirstEqualElement(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + ((right - left) >> 1)
+		if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			if (mid == 0) || (nums[mid-1] != target) { // 找到第一个与 target 相等的元素
+				return mid
+			}
+			right = mid - 1
+		}
+	}
+	return -1
+}
+
+func searchFirstGreaterElement(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + ((right - left) >> 1)
+		if nums[mid] >= target {
+			if (mid == 0) || (nums[mid-1] < target) { // 找到第一个大于等于 target 的元素
+				return mid
+			}
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
