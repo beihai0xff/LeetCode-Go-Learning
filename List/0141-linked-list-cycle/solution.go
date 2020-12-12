4da3256698129a59cd45ebce746e05207d3b2afa
beihai <beihai@wingsxdu.com>
2020-12-12 16:22:30 +0800

    添加题解 0141

---
+++ List/0141-linked-list-cycle/solution.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020.  beihai@wingsxdu.com. All right reserved
+ * ProjectName: $LeetCode
+ * LastModified: 2020/12/12 下午3:50
+ * Author: beihai
+ */
+
+package _141_linked_list_cycle
+
+// 寻找两个链表的交叉点，思路类似于链表找环
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 快慢指针
+func hasCycle(head *ListNode) bool {
+	// 这段判断可以直接丢掉
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head
+	for slow != nil && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
