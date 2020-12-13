5683a936cbcef5d89d785e62a5e62c26c58e8aa1
beihai <beihai@wingsxdu.com>
2020-12-13 21:40:58 +0800

    添加 冒泡排序 快速排序 插入排序 选择排序 归并排序

---
+++ Util/sort/sort.go
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2020.  beihai@wingsxdu.com. All right reserved
+ * ProjectName: $LeetCode
+ * LastModified: 2020/12/13 下午5:45
+ * Author: beihai
+ */
+
+package sort
+
+// 冒泡排序
+func BubbleSort(nums []int) []int {
+	flag := true
+	for i := 0; i < len(nums)-1; i++ {
+		flag = true
+
+		for j := 0; j < len(nums)-i-1; j++ { // 排序区间为 [0,len-i-1]
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+				flag = false
+			}
+		}
+
+		if flag == true { // 如果某一趟排序已经是递增的了
+			break
+		}
+	}
+	return nums
+}
+
+// 快速排序
+func QuickSort(nums []int) {
+	length := len(nums)
+	if length < 1 {
+		return
+	}
+
+	head, trip := 0, length-1
+	for head < trip {
+		if nums[head+1] > nums[head] { // 标尺元素遇到大于它的，就把这个元素和最右边 trip 交换，trip 左移一位
+			nums[head+1], nums[trip] = nums[trip], nums[head+1]
+			trip--
+		} else if nums[head+1] < nums[head] { // 标尺元素遇到小于它的，就交换位置，标尺右移动一位。
+			nums[head], nums[head+1] = nums[head+1], nums[head]
+			head++
+		} else { // 相等不用交换
+			head++
+		}
+	}
+	QuickSort(nums[:head])
+	QuickSort(nums[head+1:])
+}
+
+// 插入排序
+func InsertionSort(nums []int) {
+	if len(nums) < 1 {
+		return
+	}
+	for i := 1; i < len(nums); i++ {
+		for j := i; j >= 1 && nums[j] < nums[j-1]; { // 从左向右 逐一交换
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+			j--
+		}
+	}
+}
+
+// 选择排序：迭代中的当前元素替换为右侧的最小值
+func SelectSort(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+
+	for i := 0; i < len(nums); i++ {
+		min := i // 只比较 i 右侧的元素
+		// 从i右侧的所有元素中找出当前最小值所在的下标
+		for j := len(nums) - 1; j > i; j-- {
+			if nums[j] < nums[min] {
+				min = j
+			}
+		}
+		nums[i], nums[min] = nums[min], nums[i] // len(nums)
+	}
+}
+
+// 归并排序
+func MergeSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+	m := len(nums) / 2
+	l := MergeSort(nums[:m])
+	r := MergeSort(nums[m:])
+	return merge(l, r)
+}
+
+func merge(a, b []int) (res []int) {
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i] <= b[j] {
+			res = append(res, a[i])
+			i++
+		} else {
+			res = append(res, b[j])
+			j++
+		}
+	}
+	// 合并剩余部分
+	res = append(res, a[i:]...)
+	res = append(res, b[j:]...)
+	return
+}
---
+++ Util/sort/sort_test.go
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020.  beihai@wingsxdu.com. All right reserved
+ * ProjectName: $LeetCode
+ * LastModified: 2020/12/13 下午6:11
+ * Author: beihai
+ */
+
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test0", args{[]int{4, 1, 7, 9, 2, 5, 3}}, []int{1, 2, 3, 4, 5, 7, 9}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BubbleSort(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BubbleSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_quickSort(t *testing.T) {
+	nums := []int{5, 4, 3, 2, 1}
+	QuickSort(nums)
+	if nums[0] != 1 || nums[1] != 2 || nums[2] != 3 || nums[3] != 4 ||
+		nums[4] != 5 {
+		t.Error("QuickSort() failed. Got", nums, "Expected 1 2 3 4 5")
+	}
+}
+
+func TestInsertionSort(t *testing.T) {
+	nums := []int{5, 4, 3, 2, 1}
+	InsertionSort(nums)
+	if nums[0] != 1 || nums[1] != 2 || nums[2] != 3 || nums[3] != 4 ||
+		nums[4] != 5 {
+		t.Error("InsertionSort() failed. Got", nums, "Expected 1 2 3 4 5")
+	}
+}
+
+func TestSelectSort(t *testing.T) {
+	nums := []int{5, 4, 3, 2, 1}
+	SelectSort(nums)
+	if nums[0] != 1 || nums[1] != 2 || nums[2] != 3 || nums[3] != 4 ||
+		nums[4] != 5 {
+		t.Error("SelectSort() failed. Got", nums, "Expected 1 2 3 4 5")
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	nums := []int{5, 4, 3, 2, 1}
+	res := MergeSort(nums)
+	if res[0] != 1 || res[1] != 2 || res[2] != 3 || res[3] != 4 ||
+		res[4] != 5 {
+		t.Error("MergeSort() failed. Got", nums, "Expected 1 2 3 4 5")
+	}
+}
