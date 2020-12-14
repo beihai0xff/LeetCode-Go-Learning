083fbcf0678af32cfdef89184d764d656487c5be
beihai <beihai@wingsxdu.com>
2020-12-14 14:06:42 +0800

    添加题解 344

---
+++ String/0344-reverse-string/solution.go
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020.  beihai@wingsxdu.com. All right reserved
+ * ProjectName: $LeetCode
+ * LastModified: 2020/12/14 下午2:02
+ * Author: beihai
+ */
+
+package _344_reverse_string
+
+// 反转字符串
+
+// 双指针
+func reverseString(s []byte) {
+	left, right := 0, len(s)-1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
