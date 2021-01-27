b5539d11a2ebf65fa8b40f7271106debba145d27
wingsxdu <beihai@wingsxdu.com>
2021-01-27 21:46:38 +0800

    添加题解 0739

---
+++ 0739-daily-temperatures.go
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021.  beihai@wingsxdu.com. All right reserved
+ * ProjectName: $LeetCode
+ * LastModified: 2021/1/27 下午9:23
+ * Author: beihai
+ */
+
+package LeetCode_Go_Learing
+
+/*
+请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+
+例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+
+提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+*/
+
+// 单调栈
+func dailyTemperatures(T []int) []int {
+	length := len(T)
+	ans := make([]int, length)
+	var stack []int
+	for i := 0; i < length; i++ {
+		temperature := T[i]
+		for len(stack) > 0 && temperature > T[stack[len(stack)-1]] {
+			prevIndex := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			ans[prevIndex] = i - prevIndex
+		}
+		stack = append(stack, i)
+	}
+	return ans
+}
