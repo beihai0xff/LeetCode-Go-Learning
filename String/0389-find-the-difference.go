d002f39eb1611016b40e88336b112f12ec01bb74
wingsxdu <beihai@wingsxdu.com>
2021-01-28 22:09:03 +0800

    添加题解 0389

--- String/0389-find-the-difference.go
+++ String/0389-find-the-difference.go
@@ -31,3 +31,16 @@ func findTheDifference(s string, t string) byte {
 	}
 	return 0
 }
+
+// 异或
+func findTheDifference2(s string, t string) byte {
+	var res byte
+	for i := 0; i < len(s); i++ {
+		res ^= s[i]
+	}
+	for i := 0; i < len(t); i++ {
+		res ^= t[i]
+	}
+
+	return res
+}
