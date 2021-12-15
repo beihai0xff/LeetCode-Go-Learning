9d7d136a8ba7b441808dcba5dbc71b61b8ec5f70
beihai0xff <beihai@wingsxdu.com>
2021-12-15 19:51:04 +0800

    add offer 27

---
+++ Offer/27-er-cha-shu-de-jing-xiang.go
@@ -0,0 +1,11 @@
+package Offer
+
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	mirrorTree(root.Left)
+	mirrorTree(root.Right)
+	return root
+}
