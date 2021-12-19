76caad5b922d8c245b6748ffa5a6d48692824595
beihai0xff <beihai@wingsxdu.com>
2021-12-19 23:05:42 +0800

    add offer 42

---
+++ Offer/42-lian-xu-zi-shu-zu-de-zui-da-he.go
@@ -0,0 +1,19 @@
+package Offer
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	max := 0
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
