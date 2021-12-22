c1371aec91a8f5ff13fdd76d58a2a21f5e9a4be3
beihai0xff <beihai@wingsxdu.com>
2021-12-22 19:36:10 +0800

    add 94

---
+++ 94-binary-tree-inorder-traversa/solution.go
@@ -0,0 +1,22 @@
+package solution
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return
+}
