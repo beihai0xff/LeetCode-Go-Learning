4ad8705f4c3c6fea289c7ef65fa2d7fb2b2d1fc3
beihai0xff <beihai@wingsxdu.com>
2022-07-04 15:06:45 +0800

    add CodTop 141

---
+++ CodeTop/141. 环形链表.go
@@ -0,0 +1,18 @@
+package CodeTop
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+
+	return false
+}
