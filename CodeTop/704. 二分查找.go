0e9adf2610fb75d637a43440671b9ff4c36d49a3
beihai0xff <beihai@wingsxdu.com>
2022-07-05 19:12:09 +0800

    add CodeTop 704

---
+++ CodeTop/704. 二分查找.go
@@ -0,0 +1,22 @@
+package CodeTop
+
+func binarySearch(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
---
+++ CodeTop/704. 二分查找_test.go
@@ -0,0 +1,31 @@
+package CodeTop
+
+import "testing"
+
+func Test_binarySearch(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "normal",
+			args: args{
+				nums:   []int{-1, 0, 3, 5, 9, 12},
+				target: 9,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := binarySearch(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("binarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
