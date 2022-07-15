718988c3f9d96bf6c2d2340cbf8ccd73f150244a
beihai0xff <beihai@wingsxdu.com>
2022-07-15 20:55:48 +0800

    add CodeTop 226

---
+++ CodeTop/226. 翻转二叉树.go
@@ -0,0 +1,12 @@
+package CodeTop
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left, root.Right = right, left
+	return root
+}
