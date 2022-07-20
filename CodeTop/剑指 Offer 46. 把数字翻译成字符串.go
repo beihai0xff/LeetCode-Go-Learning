306b6a0df3468f62aa29874fcdc9527df9c2f936
beihai0xff <beihai@wingsxdu.com>
2022-07-20 12:34:16 +0800

    add CodeTop 剑指 Offer 46

--- CodeTop/剑指 Offer 46. 把数字翻译成字符串.go
+++ CodeTop/剑指 Offer 46. 把数字翻译成字符串.go
@@ -2,6 +2,12 @@ package CodeTop
 
 import "strconv"
 
+// 青蛙跳台的经典问题么（青蛙跳台阶，一次可以跳1级，也可以跳2级，问n级台阶多少种跳法)?
+//
+// f(n) = f(n-1) + f(n-2)
+// 所以动态规划，只需要知道dp[0]和dp[1]，然后递推就好了。
+//
+// 这里的变化是，能不能跳两个需要判断一下。
 func translateNum(num int) int {
 	src := strconv.Itoa(num)
 	dp0, dp1, res := 0, 1, 1
