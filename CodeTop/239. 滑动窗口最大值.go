eed94bdfdf49c6bcd3a3a9930e5a8bedcabdc350
beihai0xff <beihai@wingsxdu.com>
2022-07-20 19:57:58 +0800

    add CodeTop 239

--- CodeTop/239. 滑动窗口最大值.go
+++ CodeTop/239. 滑动窗口最大值.go
@@ -31,18 +31,27 @@ func (q *maxStack) GetMax() int {
 
 // 单调队列
 func maxSlidingWindow(nums []int, k int) []int {
-	q := maxStack{
-		cap:   k,
-		max:   []int{},
-		queue: []int{},
+	var q []int
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q)-1]] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
 	}
+
 	for i := 0; i < k; i++ {
-		q.Push(nums[i])
+		push(i)
 	}
-	res := []int{q.GetMax()}
-	for i := k; i < len(nums); i++ {
-		q.Push(nums[i])
-		res = append(res, q.GetMax())
+
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q[0]]
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i-k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
 	}
-	return res
+	return ans
 }
