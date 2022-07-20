41944c9760e44e5d61a005c275d06389daf5d74e
beihai0xff <beihai@wingsxdu.com>
2022-07-20 16:27:03 +0800

    add CodeTop 46

---
+++ CodeTop/46. 全排列.go
@@ -0,0 +1,29 @@
+package CodeTop
+
+func permute(nums []int) [][]int {
+	var res [][]int
+	visited := map[int]bool{}
+
+	var dfs func(path []int)
+	dfs = func(path []int) {
+		if len(path) == len(nums) {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			res = append(res, temp)
+			return
+		}
+		for _, n := range nums {
+			if visited[n] {
+				continue
+			}
+			path = append(path, n)
+			visited[n] = true
+			dfs(path)
+			path = path[:len(path)-1]
+			visited[n] = false
+		}
+	}
+
+	dfs([]int{})
+	return res
+}
