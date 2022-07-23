9a26239b47473bde5c874a2cf65144c9de9e8e39
beihai0xff <beihai@wingsxdu.com>
2022-07-23 14:55:46 +0800

    add CodeTop 028

---
+++ CodeTop/28. 实现 strStr().go
@@ -0,0 +1,16 @@
+package CodeTop
+
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+
+	m, n := len(haystack), len(needle)
+	for i := 0; i <= m-n; i++ {
+		if haystack[i:i+n] == needle {
+			return i
+		}
+
+	}
+	return -1
+}
