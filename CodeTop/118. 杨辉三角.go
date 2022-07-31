fbdf2543f2921ac09ebbfdcc64b808b5ab61d0f9
beihai0xff <beihai@wingsxdu.com>
2022-07-31 22:55:45 +0800

    add CodeTop 118

---
+++ CodeTop/118. 杨辉三角.go
@@ -0,0 +1,19 @@
+package CodeTop
+
+func generate(numRows int) [][]int {
+	if numRows == 0 {
+		return nil
+	}
+	res := make([][]int, numRows)
+	res[0] = []int{1}
+	for i := 1; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+		res[i][0] = 1
+		res[i][i] = 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j] + res[i-1][j-1]
+		}
+	}
+
+	return res
+}
