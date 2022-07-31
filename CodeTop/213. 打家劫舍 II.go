e5b87e80cbf7b1ca9660c445b02a94288a67f7cd
beihai0xff <beihai@wingsxdu.com>
2022-07-31 17:12:14 +0800

    add CodeTop 213

---
+++ CodeTop/213. 打家劫舍 II.go
@@ -0,0 +1,21 @@
+package CodeTop
+
+func _rob(nums []int) int {
+	first, second := nums[0], max(nums[0], nums[1])
+	for _, v := range nums[2:] {
+		first, second = second, max(first+v, second)
+	}
+	return second
+}
+
+func rob2(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+	if n == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	return max(_rob(nums[:n-1]), _rob(nums[1:]))
+}
