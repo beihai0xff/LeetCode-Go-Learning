b1c6c3c4c7161e7bf4e5fa208e066d7068d09581
beihai0xff <beihai@wingsxdu.com>
2022-08-04 23:49:50 +0800

    add CodeTop 071

---
+++ CodeTop/71. 简化路径_test.go
@@ -0,0 +1,23 @@
+package CodeTop
+
+import "testing"
+
+func Test_simplifyPath(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1", args{"/home/"}, "/home"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := simplifyPath(tt.args.path); got != tt.want {
+				t.Errorf("simplifyPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
