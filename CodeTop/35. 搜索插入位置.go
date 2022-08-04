edf760f7982c1267a2901d505225e00ef189d8f5
beihai0xff <beihai@wingsxdu.com>
2022-08-04 22:46:21 +0800

    add CodeTop 035

--- CodeTop/35. 搜索插入位置.go
+++ CodeTop/35. 搜索插入位置.go
@@ -2,15 +2,15 @@ package CodeTop
 
 func searchInsert(nums []int, target int) int {
 	left, right := 0, len(nums)-1
-	ans := len(nums)
+	res := len(nums)
 	for left <= right {
 		mid := (right-left)>>1 + left
 		if target <= nums[mid] {
-			ans = mid
+			res = mid
 			right = mid - 1
 		} else {
 			left = mid + 1
 		}
 	}
-	return ans
+	return res
 }
